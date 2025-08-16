c5976a853b3c19483cb94ce3b375d07e7986ab59
Kareena2070 <kareenayadav1088@gmail.com>
2025-08-16 17:25:31 +0530

    import profileCard file in App.jsx and add data

--- task-1(profile-card)/react-file/src/App.jsx
+++ task-1(profile-card)/react-file/src/App.jsx
@@ -1,9 +1,27 @@
-
+import profilepic from "./assets/profile.jpeg"
+import ProfileCard from "./ProfileCard"
 function App() {
  
   return (
     <>
 
+    {/* task 1.1 */}
+    <ProfileCard
+        name = "Kareena Yadav"
+        bio = "I am fulltime student at Navgurukul"
+        image = {profilepic}
+    />
+
+    <ProfileCard 
+        name="Rahul Singh"
+        bio="Loves coding and problem solving"
+        image={profilepic}
+    />
+    <ProfileCard 
+        name="Simran Kaur"
+        bio="Frontend Developer from Delhi"
+        image={profilepic}
+    />
     
 
     </>
