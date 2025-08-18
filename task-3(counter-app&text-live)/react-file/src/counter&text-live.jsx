432787b0c61c9e2950f9358edea3af5b8a48c948
Kareena2070 <kareenayadav1088@gmail.com>
2025-08-18 10:30:07 +0530

    On counter app use Conditional rendering

--- task-3(counter-app&text-live)/react-file/src/counter&text-live.jsx
+++ task-3(counter-app&text-live)/react-file/src/counter&text-live.jsx
@@ -6,11 +6,18 @@ import { useState } from "react";
 function Counter(){
     const [count, setCount] = useState(0);
 
+    let message;
+    if (count === 0) {
+    message = "Start clicking 🚀";
+    } else {
+    message = `You clicked ${count} times`;
+    }
 
     return(
         <>
         <div className="task">
             <h2 className="count-heading">Count: {count}</h2>
+            <p className="message">{message}</p>
             <div className="btn">
             <button className="btn1" onClick={() => setCount(count + 1)}>Increment</button>
             <button className="btn1" onClick={() => setCount(count - 1)}>Decrement</button>
