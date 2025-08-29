b378a287b5380e4bf9377d502ded350c485d1a84
Kareena Yadav <kareenayadav1088@gmail.com>
2025-08-29 23:17:17 +0530

    add todo list on main page

--- task11(useReduce)/use0f-useReduce/src/App.jsx
+++ task11(useReduce)/use0f-useReduce/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Counter from './useReduce'
-
+import TodoLIist from './todoList'
 import { Route, Routes, Link } from 'react-router-dom'
 
 function App() {
@@ -12,20 +12,14 @@ function App() {
         <nav>
           <ul>
             <li><Link to="/">Counter App</Link></li>
-            
+            <li><Link to="/Todo">To-Do-List</Link></li>
           </ul>
         </nav>
 
       <Routes>
         <Route path='/' element={<Counter/>}/>
-        
+         <Route path='/Todo' element={<TodoLIist/>}/> 
       </Routes>
-      
-
-
-{/* 
-      <Counter/>
-      <TodoLIist/> */}
   
     </>
   )
